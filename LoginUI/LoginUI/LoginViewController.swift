7bd2e02527e71163b61e6a3637d2d6296aede8be
Yogesh Patel <yogeshp614@gmail.com>
2021-01-22 15:53:00 +0530

    commit login ui source code.

---
+++ LoginUI/LoginUI/LoginViewController.swift
@@ -0,0 +1,30 @@
+//
+//  LoginViewController.swift
+//  LoginUI
+//
+//  Created by Yogesh Patel on 12/01/21.
+//
+
+import UIKit
+
+class LoginViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return UIScreen.main.bounds.height
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        let tableViewHeight = self.tableView.frame.height
+        let contentHeight = self.tableView.contentSize.height
+
+        let centeringInset = (tableViewHeight - contentHeight) / 2.0
+        let topInset = max(centeringInset, 0.0)
+
+        self.tableView.contentInset = UIEdgeInsets(top: topInset, left: 0.0, bottom: 0.0, right: 0.0)
+    }
+}
