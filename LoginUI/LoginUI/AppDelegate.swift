972cc5a633e9f01cb61aea2c5335a5e6e72bc56d
Yogesh Patel <yogeshp614@gmail.com>
2021-02-10 18:38:34 +0530

    commit Signup Validation code.

--- LoginUI/LoginUI/AppDelegate.swift
+++ LoginUI/LoginUI/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,6 +14,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        IQKeyboardManager.shared.enable = true
+        IQKeyboardManager.shared.keyboardDistanceFromTextField = 20
+        IQKeyboardManager.shared.enableAutoToolbar = true
         return true
     }
 
--- LoginUI/LoginUI/Signup/SignupViewController.swift
+++ LoginUI/LoginUI/Signup/SignupViewController.swift
@@ -9,18 +9,64 @@ import UIKit
 
 class SignupViewController: UITableViewController {
 
+    @IBOutlet weak var imgProfile: UIImageView!
+    @IBOutlet weak var txtUsername: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtPassword: UITextField!
+    @IBOutlet weak var txtConPassword: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(imageTapped(tapGestureRecognizer:)))
+        imgProfile.addGestureRecognizer(tapGesture)
+    }
+    
+    @objc
+    func imageTapped(tapGestureRecognizer: UITapGestureRecognizer){
+        openGallery()
+    }
+    
+    @IBAction func btnSignupClicked(_ sender: UIButton) {
+        let imgSystem = UIImage(systemName: "person.crop.circle.badge.plus")
+        
+        if imgProfile.image?.pngData() != imgSystem?.pngData(){
+            // profile image selected
+            if let email = txtEmail.text, let password = txtPassword.text, let username = txtUsername.text, let conPassword = txtConPassword.text{
+                if username == ""{
+                    print("Please enter username")
+                }else if !email.validateEmailId(){
+                    openAlert(title: "Alert", message: "Please enter valid email", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{_ in }])
+                    print("email is not valid")
+                }else if !password.validatePassword(){
+                    print("Password is not valid")
+                } else{
+                    if conPassword == ""{
+                        print("Please confirm password")
+                    }else{
+                        if password == conPassword{
+                            // navigation code
+                            print("Navigation code Yeah!")
+                        }else{
+                            print("password does not match")
+                        }
+                    }
+                }
+            }else{
+                print("Please check your details")
+            }
+        }else{
+            print("Please select profile picture")
+            openAlert(title: "Alert", message: "Please select profile picture", alertStyle: .alert, actionTitles: ["Okay"], actionStyles: [.default], actions: [{_ in }])
+        }
     }
     
     @IBAction func btnLoginClicked(_ sender: UIButton) {
         self.navigationController?.popViewController(animated: true)
     }
     
-    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return UIScreen.main.bounds.height
-    }
+//    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+//        return UIScreen.main.bounds.height
+//    }
     
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
@@ -31,5 +77,24 @@ class SignupViewController: UITableViewController {
         let topInset = max(centeringInset, 0.0)
         
         self.tableView.contentInset = UIEdgeInsets(top: topInset, left: 0.0, bottom: 0.0, right: 0.0)
+    }
+}
+
+extension SignupViewController: UINavigationControllerDelegate, UIImagePickerControllerDelegate{
+    
+    func openGallery(){
+        if UIImagePickerController.isSourceTypeAvailable(.savedPhotosAlbum){
+            let picker = UIImagePickerController()
+            picker.delegate = self
+            picker.sourceType = .savedPhotosAlbum
+            present(picker, animated: true)
+        }
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let img = info[.originalImage] as? UIImage{
+            imgProfile.image = img
+        }
+        dismiss(animated: true)
     }
 }
